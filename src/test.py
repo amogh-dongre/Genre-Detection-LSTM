6c635f1891481f83a7c968bd6bd8114a16b92519
amogh-dongre <amoghdongre16@gmail.com>
2025-04-05 13:59:25 +0530

    cleaning up the repo

--- src/test.py
+++
@@ -1,9 +0,0 @@
-import json
-
-try:
-    with open('./genre_mapping.json', 'r') as f:
-        mapping = json.load(f)
-    print("Genre mapping loaded successfully")
-except Exception as e:
-    print(f"Error loading genre mapping: {e}")
-
